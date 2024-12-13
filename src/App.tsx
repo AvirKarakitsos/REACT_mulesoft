8179c8de0358ab3bee877fdfeff762b824560a06
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-12-13 21:08:26 +0100

    feat: implementation content part 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import image_11 from './assets/images/schema_11.png'
 import image_21 from './assets/images/schema_21.png'
 import image_22 from './assets/images/schema_22.png'
+import image_31 from './assets/images/schema_31.png'
+import image_32 from './assets/images/schema_32.png'
 import Profil from './components/Profil'
 import Summary from './components/Summary'
 import { useEffect, useState } from "react"
@@ -49,7 +51,7 @@ function App() {
       <p className='content'>Nous commencerons donc l’étude par la première étape, les spécifications.</p>
 
       <h2 className='subtitle' id='spec'>Les Spécifications</h2>
-      <p className='first-content'>C’est dans la partie Design Center de la plateforme d’Anypoint qu’on pourra écrire nos spécifications. Nous allons détailler l’architecture de notre API dans un fichier raml. Nous pouvons coder à la main ou utiliser l’aide fournie qui remplie le fichier à mesure qu’on sélectionne les différentes options (figure 2-1).</p>
+      <p className='first-content'>C’est dans la partie Design Center de la plateforme d’Anypoint qu’on pourra écrire nos spécifications. Nous allons détailler l’architecture de notre API dans un fichier raml. Nous pouvons coder à la main ou utiliser l’aide fournie qui remplie le fichier à mesure qu’on sélectionne les différentes options (figure 2.1).</p>
       <figure className='image-content'>
         <picture>
           <img className='image' src={image_21} alt="Deux options pour créer notre fichier raml"/>
@@ -57,12 +59,28 @@ function App() {
         <figcaption className='image-title'>2.1 - Deux options pour créer notre fichier raml</figcaption>
       </figure>
       <p className='content'>Dans ce fichier on écrit les différentes routes, on peut utiliser des types, fournir des exemples… Une fois le fichier complété, on peut le tester grâce à l’API console. L’option Mocking Service nous délivre une url afin de le tester sur des REST Client comme Postman.</p>
-      <p className='content'>Une fois les tests passés avec succès, on peut publier notre fichier. On le retrouve dans la partie Exchange d’Anypoint. Dans cet onglet Exchange, on peut éditer la documentation de notre API afin d'écrire une description (figure 2-2).</p>
+      <p className='content'>Une fois les tests passés avec succès, on peut publier notre fichier. On le retrouve dans la partie Exchange d’Anypoint. Dans cet onglet Exchange, on peut éditer la documentation de notre API afin d'écrire une description (figure 2.2).</p>
       <figure className='image-content'>
         <picture>
           <img className='image' src={image_22} alt="Onglet Exchange d'Anypoint"/>
         </picture>
         <figcaption className='image-title'>2.2 - Onglet Exchange d'Anypoint</figcaption>
+      </figure>
+
+      <h2 className='subtitle' id='imp'>L'implémentation</h2>
+      <p className='first-content'>Pour coder l’API on se retrouve sur l’IDE Anypoint Studio. Sur la figure 3.1 est indiquée les différentes parties du projet. On sélectionne un item dans la palette (par exemple l’élément Listener) et on le fait glisser dans le canvas. Un double clique sur l’élément nous montre les configurations à effectuer.</p>
+      <figure className='image-content'>
+        <picture>
+          <img className='image' src={image_31} alt="Projet Mule"/>
+        </picture>
+        <figcaption className='image-title'>3.1 - Projet Mule</figcaption>
+      </figure>
+      <p className='content'>Listener est notre événement source. On le lie à des événements processus (Payload, Logger) afin de constituer notre Flow (figure 3.2).</p>
+      <figure className='image-content'>
+        <picture>
+          <img className='image' src={image_32} alt="Flow"/>
+        </picture>
+        <figcaption className='image-title'>3.2 - Flow</figcaption>
       </figure>
     </div>
   )
